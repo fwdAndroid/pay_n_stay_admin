15d7c20c05dbd2da7c489f10f4ec240ceb3d0bf2
fwdAndroid <fwdkaleem@gmail.com>
2022-09-12 00:32:11 +0500

    Missing fraadles

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pay_n_stay_admin/Screens/splash.dart';
 
-void main() async{
-   WidgetsFlutterBinding.ensureInitialized(); 
-    await Firebase.initializeApp();
-  
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
