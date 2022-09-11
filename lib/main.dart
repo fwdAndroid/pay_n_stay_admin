c904d575ea609b150627b76ce569a87a2bfdc59e
fwdAndroid <fwdkaleem@gmail.com>
2022-09-12 00:23:50 +0500

    Add Firebase on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pay_n_stay_admin/Screens/splash.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized(); 
+    await Firebase.initializeApp();
+  
 }
 
 class MyApp extends StatelessWidget {
